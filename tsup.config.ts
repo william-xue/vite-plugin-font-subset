01483fcca65333078ec7adfb6d95429066a4bbc2
xueyuan <xueyuan@tode.com>
2025-11-18 17:14:09 +0800

    chore: add tsup build configuration

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+	entry: ['src/index.js'],
+	format: ['cjs', 'esm'],
+	dts: true,
+	clean: true,
+	splitting: false,
+	sourcemap: false,
+	outDir: 'dist'
+})
